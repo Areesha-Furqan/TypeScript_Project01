bd658dd51c43458e576a88a319f581f2a38439ec
Areesha-Furqan <areeshawork5@gmail.com>
2025-09-26 13:38:11 +0500

    updated project02

--- project02.ts
+++ project02.ts
@@ -96,27 +96,32 @@ interface GameResult {
 }
 
 //will understand
-function main(): void {
+function main(): void { // this function doesn’t return anything, it just runs the game logic.
     console.log("WELCOME TO THE MULTI ROUND GUESSING GAME")
-    let round = 1
-    const results: GameResult[] = []
-    let playagain: string | boolean = true
+    let round = 1 // Keeps track of the current round number. Starts from 1.
+    const resultsRecord: GameResult[] = [] // Creates an empty list (array) to store results of each round. GameResult[] means this array will contain objects of type GameResult.
+    let playagain: string | boolean = true // A variable to control whether the player wants to continue. At first it’s true (so the game runs at least once).Later, it will be updated based on user’s input (Yes/No).
+    // let currentTotalScore = 0 //if we dont make the current score and dont use reduce there
 
-    while(playagain) {
-        const dificulty = selectDificulty()
-        const result = playRound(round, dificulty)
-        results.push(result)
-        const totalscore = results.reduce((sum, r) => sum+r.score, 0) 
-        console.log(`current score: ${totalscore}`)
+    while(playagain) { // A sentinal Loop, As long as playagain = true, the game continues. the increase of round, and the change in result records will be inside the while
+        const Userdificulty = selectDificulty() // this is the first thing the game will ask. so it call this func to work first and what the user answer, will store in the variable
+        const roundResult = playRound(round, Userdificulty) // now after user select the dif, the game starts by play round func's mathedalogy. since it requires the parameters so we gave it the const which holds results as an argument to it. and it will return in form of object and store in the variable.
+        resultsRecord.push(roundResult) // since the result record is as const so we will push it into it. now the round result will go and saved innto the results Record so the list will contain the roundResult Data
 
-        playagain = readline.keyInYN("\ndo you want to play again? ")
-        round++
-    }
+        // currentTotalScore += roundResult.score // if we already declared the total score outside the loop and dont use reduce func
+        const currentScore = resultsRecord.reduce( (sum, resultsRecordElement) => sum + resultsRecordElement.score, 0) // IN-LINE FUNC: have one parameter as sum containing its first value as 0 and the second para will be the list's ele [ , , , ] and the iteration through the elements of list will containue till the last element of the list. since this is the reduce fun's method. REDUCE FUNC: conntain 2 parameters, one as calc and second as a variable where the values after calculation will be saved. so we gave it its initial value 0 to store in it so that if the result record only contain one score so that will remain the same as zero add in it. but if the result record have more than one scores in it so the first score will be add in zero and save in the sum variabl (changing from 0 -> first score) then the iteration through the list continues and the 2nd score will be as the ele and sum have the first score and both will be add so 2 scores get summed and store in the total score.EXAMPLE OF CALC: the inline func's calc after => is to add sum containing value with the resultRecord list containing 'score' in it, so it will add 0 + 10, 10 + 10 etc 
+        console.log(`current score: ${currentScore}`)
+
+        playagain = readline.keyInYN("\ndo you want to play again? ") // since the keyInYN will give ans in true or false so if the user pick Y the ans will be true and stored in the playagain variable
+        round++ // thhe round variable that already have value 1, will be ++ 1->2 but when the sentinal loop go and check the playagain variable's value so if it becomes false, the loop ended and the program comes outside the while block and the increased round remain in the loop but vanished as, and the outside the while where the round variable is created and have the fixed value will still stored as 1.
+
+    }//Loop/Round EEnded
 
     console.log("\nGame Summary: ")
-    results.forEach((res)=>{console.log(`Round ${res.round} | Difficulty ${res.dificulty} | ${res.success ? "Win": "Lose"} | Attempts : ${res.atemptUsed} | Score : ${res.score}`)})
-    const finalScore = results.reduce((sum,r)=> sum + r.score, 0)
-    console.log(`Final Score for all the rounds: ${finalScore}`)
+    resultsRecord.forEach((res)=>{console.log(`Round ${res.round} | Difficulty ${res.dificulty} | ${res.success ? "Win": "Lose"} | Attempts : ${res.atemptUsed} | Score : ${res.score}`)}) // since the for each loop will go on each element of the list and prinft it but as each element of the list is also an object so we uses ${} to take the obbject's element to print in our console , so the 1st elemt of the list (obbj) will print its elemt in form that we gave it in our console, when the all elements we take to print by res.round res.score etc and it completes every elemets that we make it print, it will move to the second eeemt and to the same as res is the elemt but we used res. to take the elemts's element
+    //     comment this out: if we already declared the total score outside the loop so we can use it as it is and only print.
+    const totalScore = resultsRecord.reduce((sum,r)=> sum + r.score, 0) // Since the current score is holding the total of every score the results record have or will have, and to shhow the user every time when he lose or win and we tell him if he wants to play agaiin. but the total score at the end will again cal (duw to, not using the current score is that, it is inside the looop ) similarly as current score and will only show once when we said No to play again and we get our full summary of the game will the total scores
+    console.log(`Your Score for all the rounds: ${totalScore}`)
     console.log("\nThank You for Playing") 
 
 }
